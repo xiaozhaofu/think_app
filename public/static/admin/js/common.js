900f6595957478ebdecc3541ef8321649f448d7f
xiaozhaofu <xiaozhaofu@163.com>
2019-05-10 08:40:49 +0800

    common.js

--- public/static/admin/js/common.js
+++ public/static/admin/js/common.js
@@ -62,8 +62,8 @@ function app_del(obj) {
 
 }
 
-function app_status(obj, url) {
-    alert(url)
+function app_status(obj) {
+    
     var url = $(obj).attr('status_url');
     // alert(url)
     layer.confirm('确认要删除吗？',function(index){
